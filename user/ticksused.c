0bbbe106503a21d89207b086559af79fb5ee2b5c
jcob-sikorski <jm.siekiera@gmail.com>
2022-01-02 19:51:35 +0100

    ticksused is now minimally equipped for real-timne debugging of scheduler.

--- user/ticksused.c
+++ user/ticksused.c
@@ -1,22 +1,15 @@
-#include "types.h"
-#include "param.h"
-#include "stat.h"
+#include "kernel/types.h"
 #include "user.h"
 
 int
 main(int argc, char *argv[])
 {
-    if(argc != 2){
-        printf("usage: mytest counter");
-        exit(1);
-    }
-
     int i, x;
-
-    for(i = 1; i < atoi(argv[1]); i++){
+    printf("testing...");
+    for(i = 1; i < 1000000; i++){
         x = x + i;
     }
-    
+    printf("test ended.");
     exit(0);
     return 0;
 }
