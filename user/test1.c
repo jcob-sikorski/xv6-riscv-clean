d5cb54f7847a756f46948a5de09d6af19a8589ad
jcob-sikorski <jm.siekiera@gmail.com>
2022-01-05 14:10:07 +0100

    Changed cast from int* to uintptr_t.

--- user/test1.c
+++ user/test1.c
@@ -1,9 +1,10 @@
 #include "kernel/types.h"
 #include "user/user.h"
+#include <stdint.h>
 
 int
 main(int argc, char *argv[])
 {
-    printf("XV6_TEST_OUTPUT %d\n", (int*)main >= 0x1000);
+    printf("XV6_TEST_OUTPUT %d\n", (uintptr_t)main >= 0x1000);
     exit(0);
 }
