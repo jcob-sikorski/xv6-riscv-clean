38a7dd6775d21bd3169d0282ede0e96ecfe508a8
jcob-sikorski <jm.siekiera@gmail.com>
2022-01-08 23:09:16 +0100

    Added new command.

---
+++ user/dump_allocated.c
@@ -0,0 +1,14 @@
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    int *frames;
+    int numframes = 50;
+    dump_allocated(frames, numframes);
+    for (int i = 0; i < numframes; i++) {
+        printf("%d\n", *frames);
+        frames++;
+    }
+    return 0;
+}
