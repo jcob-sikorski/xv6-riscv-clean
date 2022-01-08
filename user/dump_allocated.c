0d6f89480ace866a7e487239a220d3689ee1b6f6
jcob-sikorski <jm.siekiera@gmail.com>
2022-01-08 23:34:33 +0100

    Fixed error "frames is used uninitialized in this function".

--- user/dump_allocated.c
+++ user/dump_allocated.c
@@ -4,11 +4,10 @@
 int
 main(int argc, char *argv[])
 {
-    int *frames;
     int numframes = 50;
-    malloc(numframes*sizeof(*frames));
+    int *frames = malloc(numframes*sizeof(*frames));;
     memset(frames, 0, sizeof(*frames));
-    
+
     dump_allocated(frames, numframes);
     for (int i = 0; i < numframes; i++) {
         printf("%d\n", *frames);
